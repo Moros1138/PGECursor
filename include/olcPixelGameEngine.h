8a3e7ad2bd2028e160f828aa40b2b07f28e56ac8
Moros1138 <moros1138@gmail.com>
2022-06-27 21:41:13 -0400

    System Cursor NOT IMPLEMENTED GLUT

--- include/olcPixelGameEngine.h
+++ include/olcPixelGameEngine.h
@@ -5760,6 +5760,12 @@ namespace olc
 
 		virtual olc::rcode SetWindowTitle(const std::string& s) override
 		{ emscripten_set_window_title(s.c_str()); return olc::OK; }
+		
+		// NOT IMPLEMENTED
+		virtual olc::rcode ShowSystemCursor(bool state) override
+		{
+			return olc::OK;
+		}
 
 		virtual olc::rcode StartSystemEventLoop() override 
 		{ return olc::OK; }
